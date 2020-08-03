55fd25cab5d645b9045c02148687610dbbd655de
Ricky Cerny <cerny.ricky@gmail.com>
2020-08-03 11:40:59 -0400

    Added the two tables to the Database

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -2,16 +2,16 @@ USE master
 GO
 
 --drop database if it exists
-IF DB_ID('final_capstone') IS NOT NULL
+IF DB_ID('PotholeTracker') IS NOT NULL
 BEGIN
-	ALTER DATABASE final_capstone SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE final_capstone;
+	ALTER DATABASE PotholeTracker SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE PotholeTracker;
 END
 
-CREATE DATABASE final_capstone
+CREATE DATABASE PotholeTracker
 GO
 
-USE final_capstone
+USE PotholeTracker
 GO
 
 --create tables
@@ -23,6 +23,42 @@ CREATE TABLE users (
 	user_role varchar(50) NOT NULL
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 )
+
+--Pothole Table
+CREATE TABLE [Pothole]
+(
+ [Id]          int IDENTITY (1, 1) NOT NULL ,
+ [Location]    nvarchar(100) NOT NULL ,
+ [DateAdded]   datetime NOT NULL ,
+ [Description] nvarchar(200) NOT NULL ,
+
+
+ CONSTRAINT [PK_pothole] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
+
+-- [PotholeStatus]
+
+CREATE TABLE [PotholeStatus]
+(
+ [Reported]   bit NOT NULL ,
+ [InProgress] bit NOT NULL ,
+ [Repaired]   bit NOT NULL ,
+ [Id]         int NOT NULL ,
+
+
+ CONSTRAINT [FK_30] FOREIGN KEY ([Id])  REFERENCES [Pothole]([Id])
+);
+GO
+
+
+CREATE NONCLUSTERED INDEX [fkIdx_30] ON [PotholeStatus] 
+ (
+  [Id] ASC
+ )
+
+GO
+
 
 --populate default data
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
