8f2188b8ece377c83d05561e2941c4e7b9d81fea
DanGoepfert <dangoepfert@gmail.com>
2020-08-04 14:12:47 -0400

    Rework form

--- vue/src/components/NewReport.vue
+++ vue/src/components/NewReport.vue
@@ -2,12 +2,15 @@
     <div>
        <h1>Report A New Pothole</h1> 
        <form action.prevent="submit">
-           <label for="location">Nearest Address:</label>
-           <input required type="text" id="location" name="location" v-model="pothole.location" placeholder="Exp: 123 Main St Cleveland OH 44112"><br><br>
+           <label for="streetNumber">Street Number:</label>
+           <input required type="text" id="streetNumber" name="streetNumber" v-model="address.streetNumber" placeholder="123"><br><br>
+           <label for="streetName">Street Name:</label>
+           <input required type="text" id="streetName" name="streetName" v-model="address.streetName" placeholder="Superior Ave"><br><br>
+            <label for="zip">Zip Code:</label>
+           <input required type="text" id="zip" name="zip" v-model="address.zip" placeholder="44112"><br><br>
            <label for="description">Pothole Description:</label>
            <input required type="text" id="description" name="description"  v-model="pothole.description" placeholder="Tell us about the pothole"><br><br>
            <input type="button" id="button" value="Submit" v-on:click="CreatePothole(pothole)">
-
        </form>
     </div>
 </template>
@@ -17,6 +20,13 @@ import APIService from '../services/APIService'
 export default {
     data(){
         return{
+            address:{
+                streetNumber: "",
+                streetName: "",
+                city: "Cleveland",
+                state: "OH",
+                zip: ""
+            },
             pothole:{
                 location: "",
                 description: ""
@@ -27,7 +37,7 @@ export default {
         CreatePothole(newPothole){
             APIService.reportPothole(newPothole)
             this.$router.push('/')
-        }
+        },
     }
 
 }
