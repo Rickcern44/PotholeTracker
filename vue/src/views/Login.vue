f5a40e6631f502a289bcee1370c3f443c947b4c3
Ricky Cerny <cerny.ricky@gmail.com>
2020-08-04 16:50:00 -0400

    Added assests folder and started grid on home page

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,48 +1,50 @@
 <template>
-  <div id="login" class="text-center">
-    <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
-      <div
-        class="alert alert-danger"
-        role="alert"
-        v-if="invalidCredentials"
-      >Invalid username and password!</div>
-      <div
-        class="alert alert-success"
-        role="alert"
-        v-if="this.$route.query.registration"
-      >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <!-- <router-link :to="{ name: 'register' }">Need an account?</router-link> -->
-      <button type="submit">Sign in</button>
-    </form>
-    <div class="content">
-      <div class="potholelist" v-for="pothole in potholes" :key="pothole.id">
-        <ul>
-          <!-- All the potholes will go in here -->
-          <potholeCard class="card" v-bind:pothole="pothole" />
-        </ul>
+  <div class="grid">
+    <div id="login" class="text-center">
+      <form class="form-signin" @submit.prevent="login">
+        <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
+        <div
+          class="alert alert-danger"
+          role="alert"
+          v-if="invalidCredentials"
+        >Invalid username and password!</div>
+        <div
+          class="alert alert-success"
+          role="alert"
+          v-if="this.$route.query.registration"
+        >Thank you for registering, please sign in.</div>
+        <label for="username" class="sr-only">Username</label>
+        <input
+          type="text"
+          id="username"
+          class="form-control"
+          placeholder="Username"
+          v-model="user.username"
+          required
+          autofocus
+        />
+        <label for="password" class="sr-only">Password</label>
+        <input
+          type="password"
+          id="password"
+          class="form-control"
+          placeholder="Password"
+          v-model="user.password"
+          required
+        />
+        <!-- <router-link :to="{ name: 'register' }">Need an account?</router-link> -->
+        <button type="submit">Sign in</button>
+      </form>
+      <div class="content">
+        <div class="potholelist" v-for="pothole in potholes" :key="pothole.id">
+          <ul>
+            <!-- All the potholes will go in here -->
+            <potholeCard class="card" v-bind:pothole="pothole" />
+          </ul>
+        </div>
       </div>
+      <router-link :to="{ name: 'report'}">Report A Pothole</router-link>
     </div>
-    <router-link :to="{ name: 'report'}">Report A Pothole</router-link>
   </div>
 </template>
 
@@ -94,16 +96,22 @@ export default {
 };
 </script>
 <style>
-.content{
+.grid{
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-template-areas: "header header" "potholes map";
+  max-height: 1vh;
+}
+.content {
   overflow-y: auto;
-  overflow-x:hidden ;
+  overflow-x: hidden;
   border: black solid 2px;
   width: 500px;
   height: 250px;
 }
-.card{
+.card {
   border: black solid 1px;
   text-align: center;
   margin: 10px;
-  }
+}
 </style>
