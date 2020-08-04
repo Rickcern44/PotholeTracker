9c7c78122d8f02279e5e4798b7b19753b59d0cf8
DanGoepfert <dangoepfert@gmail.com>
2020-08-04 16:48:53 -0400

    ZipCode dropdown menu

--- vue/src/components/NewReport.vue
+++ vue/src/components/NewReport.vue
@@ -6,17 +6,39 @@
            <input required type="text" id="streetNumber" name="streetNumber" v-model="address.streetNumber" placeholder="123"><br><br>
            <label for="streetName">Street Name:</label>
            <input required type="text" id="streetName" name="streetName" v-model="address.streetName" placeholder="Superior Ave"><br><br>
-            <label for="zip">Zip Code:</label>
+            <!-- <label for="zip">Zip Code:</label> -->
            <!-- <input required type="text" id="zip" name="zip" v-model="address.zip" placeholder="44112"><br><br>
            <label for="description">Pothole Description:</label> -->
-           <label for="cars">Choose a car:</label>
+           <label for="zip">Choose Zip Code:</label>
 
            <!-- This is the dropdown menu -->
-            <select id="cars" name="cars">
-                <option value="volvo">Volvo</option>
-                <option value="saab">Saab</option>
-                <option value="fiat">Fiat</option>
-                <option value="audi">Audi</option>
+            <select id="zips" name="Zips" v-model="address.zip">
+                <option value="44102">44102</option>
+                <option value="44103">44103</option>
+                <option value="44104">44104</option>
+                <option value="44105">44105</option> 
+                <option value="44106">44106</option>
+                <option value="44108">44108</option>
+                <option value="44109">44109</option>
+                 <option value="44110">44110</option>
+                <option value="44111">44111</option>
+                <option value="44112">44112</option>
+                <option value="44113">44113</option>
+                <option value="44114">44114</option>
+                <option value="44115">44115</option>
+                <option value="44117">44117</option>
+                <option value="44119">44119</option>
+                <option value="44120">44120</option>
+                <option value="44121">44121</option>
+                <option value="44126">44126</option>
+                <option value="44127">44127</option>
+                <option value="44128">44128</option>
+                <option value="44129">44129</option>
+                <option value="44134">44134</option>
+                <option value="44135">44135</option>
+                <option value="44142">44142</option>
+                <option value="44144">44144</option>
+
             </select><br><br>
            <input required type="text" id="description" name="description"  v-model="description" placeholder="Tell us about the pothole"><br><br>
            <input type="button" id="button" value="Submit" v-on:click="CreatePothole()">
