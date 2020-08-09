2115ea04e3bde4406a69a4c945226558654f4103
Ricky Cerny <cerny.ricky@gmail.com>
2020-08-09 16:28:02 -0400

    Made custom icon for the map. Added info box.

--- vue/src/components/MapSearch.vue
+++ vue/src/components/MapSearch.vue
@@ -1,21 +1,48 @@
 <template>
   <div>
-    <!-- <div>
+    <div>
       <h2>Search and add a pin</h2>
       <label>
         <gmap-autocomplete @place_changed="setPlace"></gmap-autocomplete>
         <button @click="addMarkerToArray()">Add</button>
       </label>
       <br />
-    </div>-->
+    </div>
     <br />
     <gmap-map :center="center" :zoom="12" style="width: 800px;  height: 600px;">
+      <!-- Here is the info window code -->
+      <gmap-info-window
+        :options="infoOptions"
+        :position="infoWindowPos"
+        :opened="infoWinOpen"
+        @closeclick="infoWinOpen=false"
+      >{{infoContent}}</gmap-info-window>
+
       <gmap-marker
+        :key="i"
+        v-for="(m,i) in markers"
+        :position="m.position"
+        :clickable="true"
+        @click="toggleInfoWindow(m,i),center= m.position"
+        icon="http://localhost:8080/construction.png"
+      ></gmap-marker>
+
+      <!-- This is a working map address bar -->
+      <!-- <gmap-marker
         :key="index"
         v-for="(m, index) in markers"
         :position="m.position"
         @click="center=m.position"
-      ></gmap-marker>
+        icon="http://localhost:8080/construction.png"
+        @mouseover="statusText = m.text"
+        @mouseout="statusText = null"
+      ></gmap-marker>-->
+
+      <div slot="visible">
+        <div
+          style="bottom: 0; left: 0; background-color: #03c6fc; color: white; position: absolute; z-index: 100"
+        >{{statusText}}</div>
+      </div>
     </gmap-map>
   </div>
 </template>
@@ -25,12 +52,23 @@ export default {
   name: "GoogleMap",
   data() {
     return {
-      // default to Montreal to keep it simple
       // change this to whatever makes sense
-      center: { lat: 41.4687, lng: -81.68739},
+      center: { lat: 41.4993, lng: -81.6944 },
       markers: [],
       places: [],
       currentPlace: null,
+      // This is the data for the maps window
+      infoContent: "",
+      infoWindowPos: null,
+      infoWinOpen: false,
+      currentMidx: null,
+      //optional: offset infowindow so it visually sits nicely on top of our marker
+      infoOptions: {
+        pixelOffset: {
+          width: 0,
+          height: -35,
+        },
+      },
     };
   },
   props: {
@@ -40,10 +78,26 @@ export default {
   mounted() {
     this.geolocate();
     // This sets the the markers to refresh every 1 second change the number after the func call to change the refresh time
-    this.interval = setInterval(() => this.addMarkerToArray(), 1000);
+    // this.interval = setInterval(() => this.addMarkerToArray(), 1000);
+    // this.addMarkerToArray();
   },
 
   methods: {
+    // Js for the info window
+    toggleInfoWindow: function (marker, idx) {
+      this.infoWindowPos = marker.position;
+      this.infoContent = marker.infoText;
+
+      //check if its the same marker that was selected if yes toggle
+      if (this.currentMidx == idx) {
+        this.infoWinOpen = !this.infoWinOpen;
+      }
+      //if different marker set infowindow to open and reset current marker index
+      else {
+        this.infoWinOpen = true;
+        this.currentMidx = idx;
+      }
+    },
     // receives a place object via the autocomplete component
     setPlace(place) {
       this.currentPlace = place;
@@ -67,8 +121,13 @@ export default {
           lat: object.latitude,
           lng: object.longitude,
         };
-        this.markers.push({ position: marker });
+        //let icon = 'https://st2.depositphotos.com/2498595/6812/v/950/depositphotos_68125511-stock-illustration-construction-works-icon-map-pin.jpg'
+        this.markers.push({
+          position: marker,
+          infoText: object.location,
+        });
         this.places.push(this.currentPlace);
+        this.markers.push();
         this.center = marker;
         this.currentPlace = null;
       });
@@ -87,3 +146,5 @@ export default {
   },
 };
 </script>
+
+
